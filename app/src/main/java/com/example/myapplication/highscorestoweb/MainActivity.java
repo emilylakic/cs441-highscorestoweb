288c20889c1d5555a7060a34027f7fa1e9d919fa
emilylakic <emilylakic@gmail.com>
2019-11-15 11:00:05 -0500

    Added text for high score, start, and different images in game

--- app/src/main/java/com/example/myapplication/highscorestoweb/MainActivity.java
+++ app/src/main/java/com/example/myapplication/highscorestoweb/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.myapplication.highscorestoweb;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView scoreLabel;
+    private TextView startLabel;
+    private ImageView box;
+    private ImageView orange;
+    private ImageView pink;
+    private ImageView black;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scoreLabel = (TextView) findViewById(R.id.scoreLabel);
+        startLabel = (TextView) findViewById(R.id.startLabel);
+        box = (ImageView) findViewById(R.id.box);
+        orange = (ImageView) findViewById(R.id.orange);
+        pink = (ImageView) findViewById(R.id.pink);
+        black = (ImageView) findViewById(R.id.black);
     }
 }
